14df8495d6c903f6e5f41270be0dcfd09a5a0c9d
MartinAchermann <martinachermann@msn.com>
2020-08-26 11:39:29 +0200

    Testphase

--- importdata.py
+++ importdata.py
@@ -1,1 +1,327 @@
-# Import Data MX One und Sirio
+import fdb
+
+class Importuserdata():
+
+
+    def __init__(self, path):
+        self.users = {}
+        self.path = path
+        self.extensiondata = []
+        self.name1data = []
+        self.dect_extensiondata = []
+        self.ip_extensiondata = []
+        self.exddpdata = []
+        self.ksddpdata = []
+        self.parallel_ringingdata = []
+        self.users = {}
+
+
+    def print_users(self):
+        for i in self.users:
+            print(self.users[i])
+
+
+    def importuserdata(self):
+
+
+        with open(self.path + "/extension") as file:
+            print("Load Extension File")
+            self.extensiondata = file.readlines()
+            print(self.extensiondata[0])
+
+        with open(self.path + "/name1") as file:
+            print("Load Extension File")
+            self.name1data = file.readlines()
+            print(self.name1data[0])
+
+        with open(self.path + "/dect_extension") as file:
+            print("Load Extension File")
+            self.dect_extensiondata = file.readlines()
+            print(self.dect_extensiondata[0])
+
+        with open(self.path + "/ip_extension") as file:
+            print("Load Extension File")
+            self.ip_extensiondata = file.readlines()
+            print(self.ip_extensiondata[0])
+
+        with open(self.path + "/EXDDP") as file:
+            print("Load Extension File")
+            self.exddpdata = file.readlines()
+            print(self.exddpdata[0])
+
+        with open(self.path + "/KSDDP") as file:
+            print("Load Extension File")
+            self.ksddpdata = file.readlines()
+            print(self.ksddpdata[0])
+
+        with open(self.path + "/parallel_ringing") as file:
+            print("Load Extension File")
+            self.parallel_ringingdata = file.readlines()
+            print(self.parallel_ringingdata[0])
+
+    #Erstellt und Sortiert das User Dictionary, fügt alle Dateien zusammen und ergänzt diese
+    def createuserdata(self):
+        # Erstellen des User Dictionary
+
+
+        # Verarbeitet die Extension Daten in das User Dictionary
+        for i in range(len(self.extensiondata)):
+            if self.extensiondata[i][0].isdigit():
+                userdata = self.extensiondata[i].split()
+
+                number = userdata[0]
+                server = userdata[2]
+                csp = userdata[3]
+                virtual = userdata[4]
+                third = userdata[13]
+                list = [number, virtual, "", "", "", csp, server, "", "", third]
+
+                if number in self.users:
+                    self.users[number] = list
+                else:
+                    self.users[number] = list
+
+
+
+        # Verarbeitet die name1 Daten in das User Dictionary
+        for i in range(len(self.name1data)):
+            if self.name1data[i][0].isdigit():
+                userdata = self.name1data[i].split()
+
+                number = userdata[0]
+                name1 = userdata[4]
+                name2 = userdata[5]
+                list = [number, "", "", name1, name2, "", "", "", "", ""]
+
+                if number in self.users:
+                    self.users[number][0] = number
+                    self.users[number][3] = name1
+                    self.users[number][4] = name2
+                else:
+                    self.users[number] = list
+
+        # Verarbeitet die dect_extension Daten in das User Dictionary
+        for i in range(len(self.dect_extensiondata)):
+            if self.dect_extensiondata[i][0].isdigit():
+                userdata = self.dect_extensiondata[i].split()
+
+                number = userdata[0]
+                list = [number, "DECT", "", "", "", "", "", "", "", ""]
+
+                if number in self.users:
+                    self.users[number][0] = number
+                    self.users[number][1] = "DECT"
+                else:
+                    self.users[number] = list
+
+        # Verarbeitet die ip_extension Daten in das User Dictionary
+        for i in range(len(self.ip_extensiondata)):
+            if self.ip_extensiondata[i][0].isdigit():
+                userdata = self.ip_extensiondata[i].split()
+
+                number = userdata[0]
+                iptype = userdata[1]
+                list = [number, iptype, "", "", "", "", "", "", "", ""]
+
+                if number in self.users:
+                    self.users[number][0] = number
+                    self.users[number][1] = iptype
+                else:
+                    self.users[number] = list
+
+        # Verarbeitet die EXDDP Daten in das User Dictionary
+        for i in range(len(self.exddpdata)):
+            if self.exddpdata[i][0].isdigit():
+                userdata = self.exddpdata[i].split()
+
+                number = userdata[0]
+                address = userdata[1]
+                cat = userdata[2]
+                list = [number, "Analog", address, "", "", cat, "", "", "", ""]
+
+                if number in self.users:
+                    self.users[number][0] = number
+                    self.users[number][1] = "Analog"
+                    self.users[number][2] = address
+                    self.users[number][5] = cat
+                else:
+                    self.users[number] = list
+
+
+        # Verarbeitet die KSDDP Daten in das User Dictionary
+        for i in range(len(self.ksddpdata)):
+            if self.ksddpdata[i][0].isdigit():
+                userdata = self.ksddpdata[i].split()
+
+                number = userdata[0]
+                address = userdata[1]
+                cat = userdata[2]
+                list = [number, "Digital", address, "", "", cat, "", "", "", ""]
+
+                if number in self.users:
+                    self.users[number][0] = number
+                    self.users[number][1] = "Digital"
+                    self.users[number][2] = address
+                    self.users[number][5] = cat
+                else:
+                    self.users[number] = list
+
+        for i in range(len(self.parallel_ringingdata)):
+            if self.parallel_ringingdata[i][0].isdigit():
+                userdata = self.parallel_ringingdata[i].split()
+
+                number = userdata[0]
+                if len(userdata) == 4:
+                    secondary1 = userdata[1]
+                    secondary2 = userdata[2]
+                    list = [number, "", "", "", "", "", "", secondary1, secondary2, ""]
+                elif len(userdata) == 3:
+                    secondary1 = userdata[1]
+                    secondary2 = ""
+                    list = [number, "", "", "", "", "", "", secondary1, "", ""]
+
+
+                if number in self.users:
+                    self.users[number][0] = number
+                    self.users[number][7] = secondary1
+                    self.users[number][8] = secondary2
+                else:
+                    self.users[number] = list
+
+
+
+        #for i in self.users:
+            #print(self.users[i])
+
+
+
+
+class Importsystemdata():
+
+    def __init__(self, path):
+        self.path = path
+        self.system = {}
+        self.ts_aboutdata = []
+
+    def importsystemdata(self):
+
+
+        with open(self.path + "/ts_about") as file:
+            print("Load Extension File")
+            self.ts_aboutdata = file.readlines()
+            print(self.ts_aboutdata[0])
+
+        with open(self.path + "/name1") as file:
+            print("Load Extension File")
+            self.name1data = file.readlines()
+            print(self.name1data[0])
+
+        with open(self.path + "/dect_extension") as file:
+            print("Load Extension File")
+            self.dect_extensiondata = file.readlines()
+            print(self.dect_extensiondata[0])
+
+        with open(self.path + "/ip_extension") as file:
+            print("Load Extension File")
+            self.ip_extensiondata = file.readlines()
+            print(self.ip_extensiondata[0])
+
+        with open(self.path + "/EXDDP") as file:
+            print("Load Extension File")
+            self.exddpdata = file.readlines()
+            print(self.exddpdata[0])
+
+        with open(self.path + "/KSDDP") as file:
+            print("Load Extension File")
+            self.ksddpdata = file.readlines()
+            print(self.ksddpdata[0])
+
+
+class Importsiriodata():
+    con = fdb.connect(dsn='localhost:c:\sirio.gdb', user='sysdba', password='masterkey')
+
+    cur = con.cursor()
+
+    cur.execute("select * from ALARM")
+    dataAlarm = cur.fetchall()
+
+    cur.execute("select * from AL_ESPA")
+    dataEspa = cur.fetchall()
+
+    cur.execute("select * from AL_AGroup")
+    dataAgroup = cur.fetchall()
+
+    cur.execute("select * from AL_REF_A")
+    dataAlRef = cur.fetchall()
+
+    cur.execute("select * from AL_REF_J")
+    dataJoRef = cur.fetchall()
+
+    cur.execute("select * from AL_JGROUP")
+    dataJgroup = cur.fetchall()
+
+    cur.execute("select * from AL_JOB")
+    dataJob = cur.fetchall()
+
+    cur.execute("select * from AL_AB_IN")
+    dataCont = cur.fetchall()
+
+    print(dataAlarm)
+    print(dataEspa)
+    print(dataAgroup)
+    print(dataAlRef)
+    print(dataJoRef)
+    print(dataJgroup)
+    print(dataJob)
+
+    # ESPA
+    for espa in dataEspa:
+        for alarm in dataAlarm:
+            if espa[5] == alarm[0]:
+                for alref in dataAlRef:
+                    if alarm[6] == alref[1]:
+                        for joref in dataJoRef:
+                            if alref[2] == joref[1]:
+                                for job in dataJob:
+                                    if joref[2] == job[0]:
+                                        print(str(alarm[1]) + " Enthält: " + str(job[1]))
+
+    # Contacts
+    # Öffner
+    for cont in dataCont:
+        # print(str(type(cont[4])) + cont[7])
+        for alarm in dataAlarm:
+            if cont[4] == alarm[0]:
+                for alref in dataAlRef:
+                    if alarm[6] == alref[1]:
+                        for joref in dataJoRef:
+                            if alref[2] == joref[1]:
+                                for job in dataJob:
+                                    if joref[2] == job[0]:
+                                        print(str(alarm[1]) + " Enthält: " + str(job[1]))
+
+    # Schliesser
+    for cont in dataCont:
+        # print(str(type(cont[4])) + cont[7])
+        for alarm in dataAlarm:
+            if cont[2] == alarm[0]:
+                for alref in dataAlRef:
+                    if alarm[6] == alref[1]:
+                        for joref in dataJoRef:
+                            if alref[2] == joref[1]:
+                                for job in dataJob:
+                                    if joref[2] == job[0]:
+                                        print(str(alarm[1]) + " Enthält: " + str(job[1]))
+
+
+
+#data = Importdata("C:\Source20200821")
+#data.importuserdata()
+#data.createuserdata()
+
+
+
+
+#data = Importdata("c:/Source")
+#data.print_users()
+
---
+++ testengine.py
@@ -0,0 +1,3 @@
+import importdata
+
+siriodata = importdata.Importsiriodata()
